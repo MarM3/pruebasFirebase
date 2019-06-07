3be57e6d1de6d89c94dc74c56bd4ce78dd41c568
Mar Mazuelas <mm.zlsmng@gmail.com>
2019-06-07 13:45:01 +0200

    Añadimos usuarios

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+// Your web app's Firebase configuration
+var config = {
+  apiKey: "AIzaSyDhMuaTZGBM4tGjSyvvmRCTf4n5dEDn-PQ",
+  authDomain: "usuarios-a35e4.firebaseapp.com",
+  databaseURL: "https://usuarios-a35e4.firebaseio.com",
+  projectId: "usuarios-a35e4",
+  storageBucket: "usuarios-a35e4.appspot.com",
+  messagingSenderId: "237602566939",
+  appId: "1:237602566939:web:ed11c6f3053b0154"
+};
+
+// Initialize Firebase
+firebase.initializeApp(config);
+// Initialize Cloud Firestore through Firebase
+var db = firebase.firestore();
+
+//Creacion de funciones para el funcionamiento del formulario de acceso y registro
 registrar.onclick = function () {
   var email = document.getElementById("email1").value;
   var psd = document.getElementById("contrasena1").value;
@@ -5,6 +22,7 @@ registrar.onclick = function () {
   firebase.auth().createUserWithEmailAndPassword(email, psd)
     .then(function () {
       confirmar();
+      $("#exampleModal").modal("hide");
     })
     .catch(function (error) {
       // Handle Errors here.
@@ -48,6 +66,14 @@ function observador() {
     } else {
       // User is signed out.
       console.log("No existe ningún usuario activo.");
+      contenido.innerHTML = `
+      <div class="alert alert-warning alert-dismissible fade show mt-3" role="alert">
+        <strong>¡Cáspitas!</strong> Esperamos verte pronto otra vez por nuestro portal.
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      `;
     }
   });
 }
@@ -57,12 +83,26 @@ function contenidosUsuarioRegistrado(usuario) {
   if (usuario.emailVerified) {
     var contenido = document.getElementById("contenido");
     contenido.innerHTML = `
-  <p>¡Bienvenido!</p>
-  <button id="cerrar">Cerrar sesión</button>
+    <div class="alert alert-success" role="alert">
+      <h4 class="alert-heading">¡Bienvenido, ${usuario.email}!</h4>
+      <p>Siéntete a gusto en nuestro portal.</p>
+      <hr>
+      <p class="mb-0">Tenemos muchos contenidos exclusivos para usuarios registrados como tú.</p>
+      <button id="cerrar" class="btn btn-info">Cerrar sesión</button>
+    </div>
+    <h2>Añadir usuarios</h2>
+    <input type="text" id="nombre" placeholder="Introduzca su nombre" class="form-control my-3" maxlenght="50"
+      pattern="[A-Za-zÑñÁÉÍÓÚáéíóúÇç\s]">
+    <input type="text" id="apellido" placeholder="Introduzca su apellido" class="form-control my-3" maxlenght="50"
+      pattern="[A-Za-zÑñÁÉÍÓÚáéíóúÇç\s]">
+    <input type="text" id="nacimiento" placeholder="Introduzca el año de su nacimiento" class="form-control my-3" maxlegth="4">
+    <button class="btn btn-info" id="guardar">Guardar</button>
   `;
 
     var ce = document.getElementById("cerrar");
     ce.addEventListener("click", cerrar, false);
+    var gu = document.getElementById("guardar");
+    gu.addEventListener("click", guardar, false);
   }
 }
 
@@ -70,7 +110,6 @@ function cerrar() {
   firebase.auth().signOut()
     .then(function () {
       console.log("Saliendo...");
-      contenido.innerHTML = "";
     })
     .catch(function (error) {
       console.log(error);
@@ -87,4 +126,20 @@ function confirmar() {
     // An error happened.
     console.log(error);
   });
+}
+
+function guardar() {
+  //Añadir documentos a la coleccion dentro de la base de datos firebase
+  var usuario = {
+    nombre: document.getElementById("nombre").value,
+    apellido: document.getElementById("apellido").value,
+    nacimiento: document.getElementById("nacimiento").value
+  };
+  db.collection("usuarios").add(usuario)
+    .then(function (docRef) {
+      console.log("Documento escrito con ID: ", docRef.id);
+    })
+    .catch(function (error) {
+      console.error("Error añadiendo el documento: ", error);
+    });
 }
